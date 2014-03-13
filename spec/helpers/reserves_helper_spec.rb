1a06aa0a6e3a1c8fcc53b5844447b0ea6d726478
Altherlex Alves <para.alves@gmail.com>
2014-03-13 09:58:59 -0300

    unit test for a help method

--- spec/helpers/reserves_helper_spec.rb
+++ spec/helpers/reserves_helper_spec.rb
@@ -16,14 +16,19 @@ describe ReservesHelper do
   	expect(head_tag.size).to eq(6)
   end
 
-	it "have only week" do
-		date = Date.new(2014,1,1)
-		expect( now(date).saturday? ).to eq(false)
-		date = Date.new(2014,1,2)
-  	expect( now(date).sunday? ).to eq(false)
+  it "have only week" do
+	date = Date.new(2014,1,1)
+	expect( now(date).saturday? ).to eq(false)
+    	date = Date.new(2014,1,2)
+    	expect( now(date).sunday? ).to eq(false)
   end
 
   it "have return label about reserve" do
-  	helper.label_with_elem(@reserve).should be_an_instance_of Array
-	end
+    	helper.label_with_elem(@reserve).should be_an_instance_of Array
+  end
+
+  it "reserve_tag" do
+        helper.reserve_tag(Date.current, Time.now.hour).should be_an_instance_of String
+  end
+
 end
